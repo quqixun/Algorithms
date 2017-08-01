f27f20bdac4487c432df806b64739b5589c15ea6
Qixun Qu <quqixun@gmail.com>
2017-08-02 01:23:13 +0800

    Re-upload all files

---
+++ JavaBasic/HelloJava/HelloJava.java
@@ -0,0 +1,13 @@
+/**
+ * @source  Algorithms Fourth Edition
+ * @title   Basic Operation on Array
+ * @Chapter 1.1 Fundamentals - Programming Model
+ */
+
+public class HelloJava
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Java!");
+    }
+}
