a37257bb26a43bec2a3b3ca3cde9e11c4a29e479
Qixun Qu <quqixun@gmail.com>
2017-08-01 14:38:27 +0800

    Re-upload HelloJava

---
+++ JavaBasic/HelloJava/src/HelloJava.java
@@ -0,0 +1,13 @@
+/**
+ * @title  My First Java Application
+ * @author Qixun Qu
+ * @date   7/31/2017
+ */
+
+public class HelloJava
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Java!");
+    }
+}
