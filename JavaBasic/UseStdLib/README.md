316fa932c662bd0e6c5dfae0202e63135c44be41
Qixun Qu <quqixun@gmail.com>
2017-08-02 11:10:04 +0800

    Upload Basic Usage of StdIn and StdOut

---
+++ JavaBasic/UseStdLib/README.md
@@ -0,0 +1,18 @@
+### Usage
+
+#### Compile
+
+javac RandomSeq.java Average.java
+
+#### Run
+
+java RandomSeq 1000 0.0 1.0
+
+java RandomSeq 1000 0.0 1.0 > data.txt
+
+java Average
+
+java Average < data.txt
+
+
+java RandomSeq 1000 0.0 1.0 | java Average
---
+++ JavaBasic/UseStdLib/RandomSeq.java
@@ -0,0 +1,18 @@
+/**
+ * @source  Algorithms Fourth Edition
+ * @title   Basic Usage of Standard Liberary
+ * @Chapter 1.1 Fundamentals - Programming Model
+ */
+
+public class RandomSeq{
+	public static void main(String[] args){
+		int N = Integer.parseInt(args[0]);
+		double lo = Double.parseDouble(args[1]);
+		double hi = Double.parseDouble(args[2]);
+
+		for (int i = 0; i < N; i++){
+			double x = StdRandom.uniform(lo, hi);
+			StdOut.printf("%.2f\n", x);
+		}
+	}
+}
