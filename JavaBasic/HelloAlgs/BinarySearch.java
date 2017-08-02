a13b5f436344e35b6bb906b6ef96cb663583f591
Qixun Qu <quqixun@gmail.com>
2017-08-02 11:44:14 +0800

    Update style

--- JavaBasic/HelloAlgs/BinarySearch.java
+++ JavaBasic/HelloAlgs/BinarySearch.java
@@ -10,27 +10,27 @@ public class BinarySearch{
     private BinarySearch() {}
 
     public static int indexof(int[] a, int key){
-	int lo = 0;
-	int hi = a.length - 1;
-	while (lo <= hi){
-	    int mid = lo + (hi - lo) / 2;
-	    if      (key < a[mid]) hi = mid - 1;
-	    else if (key > a[mid]) lo = mid + 1;
-	    else return mid;
-	}
-	return -1;
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
     }
 
     public static void main(String[] args){
-	In in = new In(args[0]);
-	int[] whitelist = in.readAllInts();
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
 
-	Arrays.sort(whitelist);
+        Arrays.sort(whitelist);
 
-	while (!StdIn.isEmpty()){
-	    int key = StdIn.readInt();
-	    if (BinarySearch.indexof(whitelist, key) == -1)
-	        StdOut.println(key);
-	}
+        while (!StdIn.isEmpty()){
+            int key = StdIn.readInt();
+            if (BinarySearch.indexof(whitelist, key) == -1)
+                StdOut.println(key);
+        }
     }
 }
--- JavaBasic/UseStdLib/Average.java
+++ JavaBasic/UseStdLib/Average.java
@@ -6,13 +6,13 @@
 
 public class Average{
     public static void main(String[] args){
-	double sum = 0.0;
-	int cnt = 0;
-	while (!StdIn.isEmpty()){
-	    sum += StdIn.readDouble();
-	    cnt++;
-	}
-	double avg = sum / cnt;
-	StdOut.printf("Average is %.5f\n", avg);
+        double sum = 0.0;
+        int cnt = 0;
+        while (!StdIn.isEmpty()){
+            sum += StdIn.readDouble();
+            cnt++;
+        }
+        double avg = sum / cnt;
+        StdOut.printf("Average is %.5f\n", avg);
     }
 }
