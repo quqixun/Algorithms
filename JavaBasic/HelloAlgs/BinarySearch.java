d1e97c0b77af6ca039d67b5db42b4aad8517d6da
屈奇勋 (Qixun Qu) <quqixun@gmail.com>
2017-08-02 11:38:38 +0800

    Update BinarySearch.java

--- JavaBasic/HelloAlgs/BinarySearch.java
+++ JavaBasic/HelloAlgs/BinarySearch.java
@@ -7,30 +7,30 @@
 import java.util.Arrays;
 
 public class BinarySearch{
-	private BinarySearch() {}
+    private BinarySearch() {}
 
-	public static int indexof(int[] a, int key){
-		int lo = 0;
-		int hi = a.length - 1;
-		while (lo <= hi){
-			int mid = lo + (hi - lo) / 2;
-			if      (key < a[mid]) hi = mid - 1;
-			else if (key > a[mid]) lo = mid + 1;
-			else return mid;
-		}
-		return -1;
+    public static int indexof(int[] a, int key){
+	int lo = 0;
+	int hi = a.length - 1;
+	while (lo <= hi){
+	    int mid = lo + (hi - lo) / 2;
+	    if      (key < a[mid]) hi = mid - 1;
+	    else if (key > a[mid]) lo = mid + 1;
+	    else return mid;
 	}
+	return -1;
+    }
 
-	public static void main(String[] args){
-		In in = new In(args[0]);
-		int[] whitelist = in.readAllInts();
+    public static void main(String[] args){
+	In in = new In(args[0]);
+	int[] whitelist = in.readAllInts();
 
-		Arrays.sort(whitelist);
+	Arrays.sort(whitelist);
 
-		while (!StdIn.isEmpty()){
-			int key = StdIn.readInt();
-			if (BinarySearch.indexof(whitelist, key) == -1)
-				StdOut.println(key);
-		}
+	while (!StdIn.isEmpty()){
+	    int key = StdIn.readInt();
+	    if (BinarySearch.indexof(whitelist, key) == -1)
+	        StdOut.println(key);
 	}
+    }
 }
