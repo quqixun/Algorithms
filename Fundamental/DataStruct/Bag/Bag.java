5dbfc39ed5763fda9b97ffed3c2421f50d924b46
Qixun Qu <quqixun@gmail.com>
2017-08-21 20:31:06 +0200

    Modify Bag

--- Fundamental/DataStruct/Bag/Bag.java
+++ Fundamental/DataStruct/Bag/Bag.java
@@ -5,33 +5,70 @@
  */
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Bag<Item> implements Iterable<Item>{
-    private Node top; // Top element of stack
-    private class Node{
-        Item item;
-        Node next;
+public class Bag<Item> implements Iterable<Item> {
+    private Node<Item> top; // Top element of stack
+    private int n;          // Number of elements
+
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
     }
 
-    public void add(Item item){
-        Node oldTop = top;
-        top = New Node();
+    public Bag() {
+        top = null;
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void add(Item item) {
+        Node<Item> oldTop = top;
+        top = new Node<Item>();
         top.item = item;
-        top = oldTop.next;
+        top.next = oldTop;
+        n++;
     }
 
-    public Iterator<Item> iterator(){
-        return new ListIterator();
+    public Iterator<Item> iterator() {
+        return new ListIterator<Item>(top);
     }
 
-    public class ListIterator implements Iterator<Item>{
-    	private Node current = top;
+    public class ListIterator<Item> implements Iterator<Item> {
+    	private Node<Item> current;
+
+        public ListIterator(Node<Item> top) {
+            current = top;
+        }
+
     	public boolean hasNext() { return current != null; }
-    	public void remove() {}
+    	public void remove()     { throw new UnsupportedOperationException(); }
+
     	public Item next(){
+            if (!hasNext()) throw new NoSuchElementException();
     	    Item item = current.item;
     	    current = current.next;
-    	    return item
+    	    return item;
     	}
+    }
+
+    public static void main(String[] args) {
+        Bag<String> bag = new Bag<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            bag.add(item);
+        }
+
+        StdOut.println("size of bag = " + bag.size());
+        for (String s : bag) {
+            StdOut.println(s);
+        }
     }
 }
